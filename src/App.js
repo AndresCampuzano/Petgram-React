52e57d3b8a97ed5137fad113a9512dac2d350c4a
Andres Campuzano <campuxano88@gmail.com>
2020-05-17 18:31:09 -0500

    Logo.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { GlobalStyles } from './GlobalStyles';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { Logo } from './components/Logo';
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
+			<Logo />
 			<ListOfCategories />
 			<ListOfPhotoCards />
 		</>
